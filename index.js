5c75d9733609c42ece1f0edc0e1a8cde8a92170d
Pratik <pr.tikgohil@gmail.com>
2020-02-22 13:27:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens =  ["Milo", "Otis", "Garfield"]//define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  return kittens.unshift(name)
+  return kittens.push(name)
 }
 function destructivelyRemoveLastKitten() {
   return kittens.pop()
